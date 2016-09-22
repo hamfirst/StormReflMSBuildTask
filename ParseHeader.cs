dd06f1d2c35bac47356565f2a0458faf6c226876
Nick Weihs <nick.weihs@gmail.com>
2016-09-21 19:04:58 -0700

    Initial checkin

---
+++ ParseHeader.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Build.Utilities;
+
+namespace StormReflMsBuildTask
+{
+  public class ParseHeader : ToolTask
+  {
+    public string ExeFile { get; set; }
+    public string[] HeaderFiles { get; set; }
+    public string[] IncludeDirs { get; set; }
+
+    public ParseHeader()
+    {
+      LogStandardErrorAsError = true;
+    }
+
+    protected override string ToolName
+    {
+      get
+      {
+        return "StormRefl";
+      }
+    }
+
+    protected override string GenerateFullPathToTool()
+    {
+      return ExeFile;
+    }
+
+    protected override string GenerateCommandLineCommands()
+    {
+      CommandLineBuilder builder = new CommandLineBuilder();
+
+      if (HeaderFiles != null)
+      {
+        foreach (var header in HeaderFiles)
+        {
+          builder.AppendFileNameIfNotNull(header);
+        }
+      }
+
+      builder.AppendSwitch("--");
+      builder.AppendSwitch("-DSTORM_REFL_PARSE");
+      builder.AppendSwitch("-x c++");
+
+      if (IncludeDirs != null)
+      {
+        foreach (var include in IncludeDirs)
+        {
+          builder.AppendSwitch("-I" + include);
+        }
+      }
+
+      builder.AppendSwitch("-Wno-pragma-once-outside-header");
+      return builder.ToString();
+    }
+  }
+}
